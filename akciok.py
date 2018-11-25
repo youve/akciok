ba5e42bc8cbe5834b7760c8912cb34de0d3bd578
Youve <minny.molly@gmx.com>
2018-11-25 10:20:52 +0100

    loads or creates the files it needs, learns which html elements to watch for for each site

--- akciok.py
+++ akciok.py
@@ -5,34 +5,98 @@
 import json
 import argparse
 import sys
+import os
 import random
 import pprint
 import readline
 import requests
+import getkey
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys    
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import logging
 #logging.disable()
-logging.basicConfig(filename=f'log-akciok.txt', level=logging.DEBUG, format='
-%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s') #filename=f'log-akciok.txt', 
 logging.debug("Start of program.")
 
-websites = {'tesco' : ['https://tesco.hu/akciok/akcios-termekek/', '?sort=cheapest']
-            'auchan' : ['https://www.auchan.hu/akcio/', '/mind/48',]
-            'lidl' : ['https://www.lidl.hu/hu/Ajanlataink.htm', '']
-            'penny': ['https://www.penny.hu/offers', '']
-            'aldi' : ['https://www.aldi.hu/hu/ajanlatok', '']
-            #'real' : "http://real.hu/",  #requires OCR
-            #'spar' : "https://sparajanlatok.kolrus.cloud/akcios_ujsag/spar-akcios-ujsag-DATE-DATEPLUSSEVENDAYS/2850/PAGENUMBER", # requies OCR
-            #'coop' : "http://www.coop.hu/", #requires OCR
-            }
+parser = argparse.ArgumentParser(description='Scrape specials from grocery store websites.')
+parser.add_argument('-b', '--buspass', dest="buspass", action='store_true', help='Do you have a buspass?')
+parser.add_argument('-m', '--meat', dest='meat', type=float, help='How many kg meat?', nargs='?', default='1')
+parser.add_argument('-v', '--veg', dest='veg', type=float, help='How many kg vegetables?', nargs='?', default='1')
+args = parser.parse_args()
+logging.debug(f"args: {args}")
 
-#PARSE ARGS: WEBSITES TO TRAVERSE, HOW MANY KG MEAT, HOW MANY KG VEGETABLES, BUSPASS = TRUE/FALSE
+def getDirectory():
+    '''return the directory where the files are stored'''
+    abspath = os.path.abspath(__file__)
+    fileDirectory = os.path.dirname(abspath) + '/files/'
+    return fileDirectory
 
-#LOAD JSON FILES
-    # USER'S PREVIOUS SHOPPING PREFERENCES
+def loadFiles():
+    """Load all the files in the files directory.  Return a dict of files and
+    their contents.
+    """
+    jsonFiles = os.listdir(getDirectory())
+    jsonFiles.sort()
+    allFiles = {}
+    for file in jsonFiles:
+        with open(getDirectory() + file, "r") as f:
+            allFiles[file] = json.loads(f.readline().rstrip())
+    return allFiles
+
+def saveFiles(files):
+    '''Save everything we learned in json format for next time'''
+    for file in files:
+        with open(getDirectory() + file, "w") as f:
+            f.write(json.dumps(files[file]))
+
+def setupFiles(files):
+    '''Create all the files we need'''
+    logging.debug(f'Files: {files}')
+    while not files['websites'] or again("Add a new website? [y/N]"):
+        siteName = input('Name of site: ')
+        urlHead = input('Enter a URL to visit: ')
+        urlTail = input('What should be appended to URLs from this site? (e.g. ?sort=cheapest')
+        files['websites'][siteName] = [urlHead, urlTail]
+    if 'parsewebsite' not in files.keys():
+        files['parsewebsite'] = {}
+    for website in files['websites']:
+        logging.debug(f'Files["websites"]: {files["websites"]}')
+        logging.debug(f'website: {website}')
+        if website not in files['parsewebsite'].keys():
+            print(f'\nMissing parsing data for {website.capitalize()}:\n')
+            files['parsewebsite'][website] = adjustWebsiteParser()
+            logging.debug(f'files["parsewebsite"]: {files["parsewebsite"]}')
+            #why is updating all the websites instead of just the one
+    if 'blacklist' not in files.keys(): # ['eggplant', 'liver']
+        files['blacklist'] = []
+    if 'whitelist' not in files.keys(): # {'onion' : 150, 'chicken breast' : 1200}
+        files['whitelist'] = {}
+    return files
+
+def adjustWebsiteParser(website={}):
+    '''What tags to look for in each website + whether a bus pass is needed to access the store'''
+    website['itemdelineator'] = input('Html to look for at the start of each item: ')
+    website['unitprice'] = input('Html element for unitprice: ')
+    website['kgprice'] = input('Html element for kgprice: ')
+    website['itemname'] = input('Html element for item name ')
+    website['categories'] = input('Html element for categories: ')
+    website['buspass'] = again('Buspass required for store? [y/N] ')
+    return website
+
+def again(msg='Again? [y/N]', default="no"):
+    '''return true or false'''
+    whatDo = input(msg + ' ') or default
+    if whatDo[0].upper() == 'yes'.upper():
+        return True
+    else:
+        return False
+
+memory = setupFiles(loadFiles())
+logging.debug(pprint.pprint(memory))
+saveFiles(memory)
+
     # HOW TO PARSE EACH SITE
             #TESCO 
             #<div class="title">
@@ -45,9 +109,9 @@ websites = {'tesco' : ['https://tesco.hu/akciok/akcios-termekek/', '?sort=cheape
 
             # pseudocode:
             # tesco = {'itemdelineator' : '<div class="a-productListing__productsGrid__element">',
-            #'unitprice' : '<b class="new-price">',
-            #    'kgprice' : '<span class="description">(1 kg = \d+)<span>',
-            #   'itemname' : '<b class="name">',
+            #'unitprice' : '<b class="new-price">PRICE',
+            #    'kgprice' : '<span class="description">(1 kg = PRICE)<span>',
+            #   'itemname' : '<b class="name">NAME',
             #   'categories' : '<li class="a-categories__list__item ">'
             #    'buspass' : False,
             # }
@@ -61,7 +125,7 @@ websites = {'tesco' : ['https://tesco.hu/akciok/akcios-termekek/', '?sort=cheape
             #
             # auchan = { 'itemdelineator' : '<div class="box p10px prodItem">',
             #    'unitprice' : '<strong class="red featuredPrice">',
-            #    'kgprice' : ' <p>Kiszerelés:\dg<br>', # do some math to this
+            #    'kgprice' : ' <p>Kiszerelés:PRICEg<br>', # do some math to this
             #   'itemname' : '<div class=box p10px prodItem><a href>',
             #   'categories' : '<ul class="submenu"><li class="">'
             #    'buspass' : True,
@@ -83,14 +147,8 @@ websites = {'tesco' : ['https://tesco.hu/akciok/akcios-termekek/', '?sort=cheape
             #
             # PENNY
             # penny = {'itemdelineator' : '<div class="card card-hover h-100 rounded-0 cursor-pointer" data-toggle="modal" data-target="#productDetailsModal1">',
-            # 'unitprice' : '<div class="col-8 px-0 penny-red-well-color h3 d-block offers-module-text-price-font">
-            #                                <div class="">
-            #                                    <strong>PRICE Ft</strong>',
-            #    'kgprice' : '<div class="text-muted col-12 px-0 h6 d-block text-right offers-module-text-font" style="height: 17px;">
-            #                                
-            #                                
-            #                                    
-            #                                        <span>100</span> <span>gramm</span> = <span>100</span>', # do some math to this
+            # 'unitprice' : '<div class="col-8 px-0 penny-red-well-color h3 d-block offers-module-text-price-font">\n<div class="">\n<strong>PRICE Ft</strong>',
+            #    'kgprice' : '<div class="text-muted col-12 px-0 h6 d-block text-right offers-module-text-font" style="height: 17px;">\n<span>100</span> <span>gramm</span> = <span>100</span>', # do some math to this
             #   'itemname' : '<h5 class="m-0 well-dark-grey-color modal-article-title bigger-line-height">NAME</h5>',
             #   'categories' : None,
             #    'buspass' : False,
@@ -99,47 +157,47 @@ websites = {'tesco' : ['https://tesco.hu/akciok/akcios-termekek/', '?sort=cheape
             # ALDI
             # aldi = {'itemdelineator' : '<a class="box--wrapper ym-gl ym-g25 " title="Tovább a termék leírásához">'}
             # 'unitprice' : '<span class="box--value">',
-            #    'kgprice' : '<span class="box--baseprice">1 398 Ft/kg</span>', # do some math
+            #    'kgprice' : '<span class="box--baseprice">PRICE Ft/kg</span>', # do some math
             #   'itemname' : '<div class="box--description--header">NAME</div>',
             #   'categories' : '<li class="footer-sitemap--list--item dropdown--list--item">',
             #    'buspass' : True,
             #}
 
-#FOREACH WEBSITE
+#TODO: For each website
 
-    #LOAD WEBSITE
+    #load the website
 
-    #DETECT CATEGORIES
-        #IF CATEGORY NOT IN BLACKLIST
-            #IF CATEGORY NOT IN WHITELIST
-                #ASK ABOUT CATEGORY
-            #IF CATEGORY IN WHITELIST
-                #TRAVERSE CATEGORY
+    #detect categories
+        #if category not in blacklist
+            #if category not in whitelist
+                #ask about category
+            #if category in whitelist
+                #traverse category
 
 
-    #TRAVERSE CATEGORIES
+    #traverse categories
 
-        #IF ITEM NOT IN BLACKLIST            
-            #IF ITEM NOT IN WHITELIST
-                #ASK ABOUT ITEM: 
-                    #YES AT THIS PRICE
-                    #YES AT THIS PRICE BUT NOT THIS WEEK
-                    #YES BUT NOT THIS BRAND
-                    #YES BUT ONLY AT LOWER PRICE
-                    #NO NEVER
-                    #STORE ENGLISH NAME FOR SUPERCOOK
-                    #SAVE JSON FILE
-                #LEARN CATEGORY
-            #IF ITEM IN WHITELIST AND PRICE OKAY
-                #ADD ITEM TO POTENTIAL LIST
+        #if item not in blacklist            
+            #if item not in whitelist
+                #ask about item: 
+                    #yes at this price
+                    #yes at this price but not this week
+                    #yes but not this brand
+                    #yes but only at lower price
+                    #no never
+                    #store english name for supercook
+                    #save json file
+                #learn category
+            #if item in whitelist and price okay
+                #add item to potential list
 
-    #COMPILE 2-3 SAMPLE MENUS OF X KG MEAT, Y KG VEGETABLES, Z MISCELLANEOUS ITEMS
-    #SEARCH SUPERCOOK FOR RECIPES
+    #compile 2-3 sample menus of x kg meat, y kg vegetables, z miscellaneous items
+    #search supercook for recipes
 
-#SEARCH SUPERCOOK
+#search supercook
     #https://www.supercook.com/#/recipes
 
-#SORT MENUS BY CHEAPEST TOTAL PRICE
+#sort menus by cheapest total price
 
-#FOREACH MENU
-    #DISPLAY STORE, SHOPPING LIST, PRICE, SUPERCOOK RECIPES
+#foreach menu
+    #display store, shopping list, price, supercook recipes
